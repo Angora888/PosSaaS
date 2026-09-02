eb06f703c745dfe0f9b018efa135cf3bce01165e
Angora888 <angora88@gmail.com>
2026-09-01 18:18:57 -0600

    Add secure admin account recovery DTO

---
+++ PosSaaS.Api/PosSaaS.Api/DTOs/RecoverAdminDto.cs
@@ -0,0 +1,8 @@
+namespace PosSaaS.Api.DTOs
+{
+    public class RecoverAdminDto
+    {
+        public string Email { get; set; } = string.Empty;
+        public string NuevaPassword { get; set; } = string.Empty;
+    }
+}
