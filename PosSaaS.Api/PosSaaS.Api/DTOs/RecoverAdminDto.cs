9f8d5a4f001e4ab3bbc37d9c3e0d431e5a1c8802
Angora888 <angora88@gmail.com>
2026-09-01 18:31:42 -0600

    Remove temporary recovery DTO

--- PosSaaS.Api/PosSaaS.Api/DTOs/RecoverAdminDto.cs
+++
@@ -1,8 +0,0 @@
-namespace PosSaaS.Api.DTOs
-{
-    public class RecoverAdminDto
-    {
-        public string Email { get; set; } = string.Empty;
-        public string NuevaPassword { get; set; } = string.Empty;
-    }
-}
